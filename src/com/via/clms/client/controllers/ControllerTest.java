a659c37fc04f69ac517446001827b62e6e8e9576
Angel Petrov <angelilianov@gmail.com>
2018-03-28 16:39:56 +0200

    Main Controller Tester class added
    Unfinalized. Some options are not working.

---
+++ src/com/via/clms/client/controllers/ControllerTest.java
@@ -0,0 +1,109 @@
+package com.via.clms.client.controllers;
+
+import java.util.Scanner;
+
+public class ControllerTest {
+
+	Scanner keyboard = new Scanner(System.in);
+
+	public int selection;
+	public String controller;
+	public boolean loop;
+
+	public ControllerTest() {
+
+		System.out.println("Controller selection: ");
+		System.out.println("0) Exit application");
+		System.out.println("1) BookOverviewController");
+		System.out.println("2) HomeController");
+		System.out.println("3) LoginController");
+		System.out.println("4) ProfileController");
+		System.out.println("5) SearchResultController [UNAVAILABLE]");
+		System.out.println("6) RentBookController");
+		System.out.println("7) ReturnBookController");
+		System.out.println("8) RenewBookController [UNAVAILABLE]");
+		System.out.println("9) LibrarianHomeController [UNAVAILABLE]");
+		System.out.println("10) InventoryManagementController [UNAVAILABLE]");
+		System.out.println("11) ManageUsersController [UNAVAILABLE]");
+		System.out.println("12) SystemSettingsController [UNAVAILABLE]");
+
+
+		selection = keyboard.nextInt();
+		logicHandler();
+
+	}
+
+	public int logicHandler() {
+
+		boolean invalidInput = false;
+		while (!invalidInput) {
+			if (selection == 0) {
+				// Exit
+				System.out.println("Application terminated.");
+				Runtime.getRuntime().exit(0);
+			}
+			if (selection == 1) {
+				System.out.println("BookOverviewController now running");
+				controller = "BookOverviewController";
+				invalidInput = true;
+			}
+			if (selection == 2) {
+				System.out.println("HomeController now running");
+				controller = "HomeController";
+				invalidInput = true;
+			}
+			if (selection == 3) {
+				System.out.println("LoginController now running");
+				controller = "LoginController";
+				invalidInput = true;
+			}
+			if (selection == 4) {
+				System.out.println("ProfileController now running");
+				controller = "ProfileController";
+				invalidInput = true;
+			}
+			if (selection == 5) {
+				System.out.println("SearchResultController now running");
+				controller = "SearchResultController";
+				invalidInput = true;
+			}
+			if (selection == 6) {
+				System.out.println("RentBookController now running");
+				controller = "RentBookController";
+				invalidInput = true;
+			}
+			if (selection == 7) {
+				System.out.println("ReturnBookController now running");
+				controller = "ReturnBookController";
+				invalidInput = true;
+			}
+			if (selection == 8) {
+				System.out.println("RenewBookController now running");
+				controller = "RenewBookController";
+				invalidInput = true;
+			}
+			if (selection == 9) {
+				System.out.println("LibrarianHomeController now running");
+				invalidInput = true;
+			}
+			if (selection == 10) {
+				System.out.println("InventoryManagementController now running");
+				invalidInput = true;
+			}
+			if (selection == 11) {
+				System.out.println("ManageUsersController now running");
+				invalidInput = true;
+			}
+			if (selection == 12) {
+				System.out.println("SystemSettingsController now running");
+				invalidInput = true;
+			}
+		}
+		if (selection > 12 || selection < 1) {
+			System.out.println("Error, enter valid choice!");
+			selection = keyboard.nextInt();
+
+		}
+		return selection;
+	}
+}
---
+++ src/com/via/clms/client/controllers/ControllerTesterStart.java
@@ -0,0 +1,160 @@
+package com.via.clms.client.controllers;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ControllerTesterStart extends Application {
+
+	public static ControllerTest debugger;
+	public static String controllerName;
+
+	public ControllerTesterStart() {
+		debugger = new ControllerTest();
+		controllerName = debugger.controller;
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		if (controllerName == "BookOverviewController") {
+			try {
+				BookOverviewController bookOverviewController = new BookOverviewController();
+				primaryStage.setScene(new Scene(bookOverviewController.getComponent()));
+				primaryStage.setTitle(bookOverviewController.getTitle());
+				primaryStage.show();
+			} catch (Exception e) {
+				System.out.println("Error in creating controller object.");
+				Runtime.getRuntime().exit(0);
+			}
+		}
+		if (controllerName == "HomeController") {
+			try {
+				HomeController homeController = new HomeController();
+				primaryStage.setScene(new Scene(homeController.getComponent()));
+				primaryStage.setTitle(homeController.getTitle());
+				primaryStage.show();
+			} catch (Exception e) {
+				System.out.println("Error in creating controller object.");
+				Runtime.getRuntime().exit(0);
+			}
+		}
+		if (controllerName == "LoginController") {
+			try {
+				LoginController loginController = new LoginController();
+				primaryStage.setScene(new Scene(loginController.getComponent()));
+				primaryStage.setTitle(loginController.getTitle());
+				primaryStage.show();
+			} catch (Exception e) {
+				System.out.println("Error in creating controller object.");
+				Runtime.getRuntime().exit(0);
+			}
+		}
+		if (controllerName == "ProfileController") {
+			try {
+				ProfileController profileController = new ProfileController(392613);
+				primaryStage.setScene(new Scene(profileController.getComponent()));
+				primaryStage.setTitle(profileController.getTitle());
+				primaryStage.show();
+			} catch (Exception e) {
+				System.out.println("Error in creating controller object.");
+				Runtime.getRuntime().exit(0);
+			}
+		}
+//		if (controllerName == "SearchResultController") {
+//			try {
+//				SearchResultController searchResultController = new SearchResultController();
+//				primaryStage.setScene(new Scene(searchResultController.getComponent()));
+//				primaryStage.setTitle(searchResultController.getTitle());
+//				primaryStage.show();
+//			} catch (Exception e) {
+//				System.out.println("Error in creating controller object.");
+//				Runtime.getRuntime().exit(0);
+//			}
+//		}
+		if (controllerName == "RentBookController") {
+			try {
+				RentBookController rentBookController = new RentBookController();
+				primaryStage.setScene(new Scene(rentBookController.getComponent()));
+				primaryStage.setTitle(rentBookController.getTitle());
+				primaryStage.show();
+			} catch (Exception e) {
+				System.out.println("Error in creating controller object.");
+				Runtime.getRuntime().exit(0);
+			}
+		}
+		if (controllerName == "ReturnBookController") {
+			try {
+				ReturnBookController returnBookController = new ReturnBookController();
+				primaryStage.setScene(new Scene(returnBookController.getComponent()));
+				primaryStage.setTitle(returnBookController.getTitle());
+				primaryStage.show();
+			} catch (Exception e) {
+				System.out.println("Error in creating controller object.");
+				Runtime.getRuntime().exit(0);
+			}
+		}
+//		if (controllerName == "RenewBookController") {
+//			try {
+//				RenewBookController renewBookController = new RenewBookController();
+//				primaryStage.setScene(new Scene(renewBookController.getComponent()));
+//				primaryStage.setTitle(renewBookController.getTitle());
+//				primaryStage.show();
+//			} catch (Exception e) {
+//				System.out.println("Error in creating controller object.");
+//				Runtime.getRuntime().exit(0);
+//			}
+//		}
+//		if (controllerName == "LibrarianHomeController") {
+//		try {
+//			LibrarianHomeController librarianHomeController = new LibrarianHomeController();
+//			primaryStage.setScene(new Scene(librarianHomeController.getComponent()));
+//			primaryStage.setTitle(librarianHomeController.getTitle());
+//			primaryStage.show();
+//		} catch (Exception e) {
+//			System.out.println("Error in creating controller object.");
+//			Runtime.getRuntime().exit(0);
+//		}
+//	}	
+//		if (controllerName == "InventoryManagementController") {
+//		try {
+//			InventoryManagementController inventoryManagementController = new InventoryManagementController();
+//			primaryStage.setScene(new Scene(inventoryManagementController.getComponent()));
+//			primaryStage.setTitle(inventoryManagementController.getTitle());
+//			primaryStage.show();
+//		} catch (Exception e) {
+//			System.out.println("Error in creating controller object.");
+//			Runtime.getRuntime().exit(0);
+//		}
+//	}
+//		if (controllerName == "ManageUsersController") {
+//		try {
+//			InventoryManagementController manageUsersController = new InventoryManagementController();
+//			primaryStage.setScene(new Scene(manageUsersController.getComponent()));
+//			primaryStage.setTitle(manageUsersController.getTitle());
+//			primaryStage.show();
+//		} catch (Exception e) {
+//			System.out.println("Error in creating controller object.");
+//			Runtime.getRuntime().exit(0);
+//		}
+//	}
+//		if (controllerName == "SystemSettingsController") {
+//		try {
+//			SystemSettingsController systemSettingsController = new SystemSettingController();
+//			primaryStage.setScene(new Scene(systemSettingsController.getComponent()));
+//			primaryStage.setTitle(systemSettingsController.getTitle());
+//			primaryStage.show();
+//		} catch (Exception e) {
+//			System.out.println("Error in creating controller object.");
+//			Runtime.getRuntime().exit(0);
+//		}
+
+	}
+
+	public static void main(String[] args) {
+
+		Application.launch(controllerName);
+
+	}
+}
