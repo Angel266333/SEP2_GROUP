d9b9491e51f89e438f9c962d8406fb40e5186a39
DiegoHisen <36505682+DiegoHisen@users.noreply.github.com>
2018-03-28 13:19:40 +0200

    Update ReturnBookController.java

--- src/com/via/clms/client/controllers/ReturnBookController.java
+++ src/com/via/clms/client/controllers/ReturnBookController.java
@@ -8,42 +8,29 @@ import javafx.stage.Window;
 
 public class ReturnBookController implements Controller {
    private GridPane mainPane;
-   private long bookID;
-   private String bookName;
 
-   private String name;
 
-   public ReturnBookController(long bookID) {
-      this.bookID = bookID;
+   public ReturnBookController() {
+      
 
       mainPane = new GridPane();
-      getData();
 
 
       String labelStyle = "-fx-font-weight: bold";
-      Label nameLabel = new Label("Name  ");
-      nameLabel.setStyle(labelStyle);
-      Label nameTextLabel = new Label(name);
       Label bookIDLabel = new Label("Book ID  ");
-      Label bookTitleLabel = new Label("Book Title  ");
-      bookTitleLabel.setStyle(labelStyle);
-      Label titleTextLabel = new Label(bookName);
 
+      TextField bookIDText = new TextField();
 
       bookIDLabel.setStyle(labelStyle);
-      Label bookIDTextLabel = new Label("" + bookID);
 
 
       Button returnButton = new Button("  Return the book  ");
 
-      
-      mainPane.add(nameLabel, 0, 0);
-      mainPane.add(nameTextLabel, 2, 0);
-      mainPane.add(bookIDLabel, 0, 1);
-      mainPane.add(bookIDTextLabel, 2, 1);
-      mainPane.add(bookTitleLabel, 0, 2);
-      mainPane.add(titleTextLabel, 2, 2);
-      mainPane.add(returnButton, 1, 3);
+
+      mainPane.add(bookIDLabel, 0, 0);
+      mainPane.add(bookIDText, 0, 1);
+
+      mainPane.add(returnButton, 1, 1);
    }
 
    public String getTitle() {
@@ -66,11 +53,4 @@ public class ReturnBookController implements Controller {
 
    }
 
-   private void getData() {
-      //Test data REMOVE====
-      name = "John Hansen";
-      bookName = "The Da Vinci Code";
-      bookID = 45678;
-
-   }
 }
