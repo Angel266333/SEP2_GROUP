b49dac4496eaf5d4e21c48eec205aa39801102c1
Angel Petrov <angelilianov@gmail.com>
2018-04-09 01:09:28 +0200

    Improved SearchResultController
    Now has the option to display results properly in TableView.

--- src/com/via/clms/client/controllers/SearchResultController.java
+++ src/com/via/clms/client/controllers/SearchResultController.java
@@ -3,24 +3,34 @@ package com.via.clms.client.controllers;
 import com.via.clms.client.views.Controller;
 import com.via.clms.client.views.Window;
 
-import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-public class SearchResultController implements Controller {
+public class SearchResultController implements Controller {	
+	
+	/*
+	 * Credits for code example: Oracle Corporation.
+	 * Example used:
+	 * https://docs.oracle.com/javafx/2/ui_controls/table-view.htm
+	 * 
+	 */	
+	
+	private SearchResultData dataOutput;
+
 	private GridPane mainPane;
 	private GridPane searchPane;
 	private GridPane labelBookSearchFiltersPane;
@@ -39,23 +49,30 @@ public class SearchResultController implements Controller {
 	
 	private Label lbl1Search;
 	private Label lbl2BookOptions;
-	private Label lbl3SortResultsCriterias;
 	private Label lbl3SearchResults;
 
 	// For future reference, this element can implement ArrayLists.
-	private ComboBox cb1BookGenre = new ComboBox();
-	private ComboBox cb2BookYearFilter = new ComboBox();
-	private ComboBox cb3BookLanguage = new ComboBox();
-	private ComboBox cb4BookLibraryLocation = new ComboBox();
+	public ComboBox cb1BookGenre;
+	public ComboBox cb2BookYear;
+	public ComboBox cb3BookLanguage;
+	public ComboBox cb4BookLibraryLocation;
 	
-	// For future reference, this element can implement ArrayLists.
-	private TableView tbView1BookResults;
+	public String bookName;
+	public String bookAuthor;
+	public String bookYear;
+	public String bookAvailability;
 	
 	
-    TableColumn bookNameCol1;
-    TableColumn bookAuthorNameCol2;
-    TableColumn bookdateOfIssueCol3;
-    TableColumn bookAvailabilityCol4;
+	private TableView<SearchResultData> tbView1BookResults;
+	private final ObservableList<SearchResultData> data = FXCollections.observableArrayList(
+	new SearchResultData("The Ugly Duckling", "Hans Christian Andersen", "1843", "3"),
+	new SearchResultData("A Daughter of Thought", "Maryana Marrash", "1893", "2"),
+	new SearchResultData("The Iron Candlestick", "Dimitar Talev", "1952", "1"));
+	
+    private TableColumn<SearchResultData, String> bookNameCol1;
+    private TableColumn<SearchResultData, String> bookAuthorNameCol2;
+    private TableColumn<SearchResultData, String> bookYearCol3;
+    private TableColumn<SearchResultData, String> bookAvailabilityCol4;
     
 	private Button btn1Search;
 	private Button btn2RentSelectedBooks;
@@ -63,6 +80,7 @@ public class SearchResultController implements Controller {
 	private Button btn4MyProfile;
 	private Button btn5HomeSection;	
 	
+
 	public SearchResultController() {		
 				
 		mainPane = new GridPane();
@@ -79,38 +97,46 @@ public class SearchResultController implements Controller {
 		lbl2BookOptions = new Label("Book search filters:");
 		lbl2BookOptions.setPadding(new Insets(0, 0, 5, 0));
 		lbl3SearchResults = new Label("Search results:");
-		lbl3SearchResults.setPadding(new Insets(0, 0, 5, 27));
+		lbl3SearchResults.setPadding(new Insets(0, 0, 5, 40));
 		
 		// Placeholders. Actual Strings need to be retrieved from
 		// database where user has, for example, specified that
 		// there are 2 libraries with location "Vejle", "Malmo" etc.
 		
-		cb1BookGenre.setPromptText("Select genre");
+		cb1BookGenre = new ComboBox();
+		cb1BookGenre.setPromptText("Book genre");
+		cb1BookGenre.setPrefWidth(135);
 	     cb1BookGenre.getItems().addAll(
 	     "Action", "Thriller", "Comedy",
 	     "Horror", "Sci-Fi", "Culture",
 	     "Histoty", "Fantasy", "Mythology"
 	     );
 	     
-	     cb2BookYearFilter.setPromptText("Select year");
-	     cb2BookYearFilter.getItems().addAll(
+	     cb2BookYear = new ComboBox();
+	     cb2BookYear.setPromptText("Book year");
+	     cb2BookYear.setPrefWidth(135);
+	     cb2BookYear.getItems().addAll(
 	    "2018", "2017", "2016", "2015", "2014",
 	    "2013", "2012", "2011", "2010", "2009",
 	    "2008", "2007", "2006", "2005", "2004",
 	    "2003", "2002", "2001", "2000", "Before 2000"
 	    );
 	     
+	     cb3BookLanguage = new ComboBox();
 	     cb3BookLanguage.setPromptText("Book language");
+	     cb3BookLanguage.setPrefWidth(135);
 	     cb3BookLanguage.getItems().addAll(
 	    "English", "Danish", "Bulgarian","German",
 	    "Dutch", "French", "Russian", "Polish");
 	     
+	     cb4BookLibraryLocation = new ComboBox();
 	     cb4BookLibraryLocation.setPromptText("Book location");
+	     cb4BookLibraryLocation.setPrefWidth(135);
 	     cb4BookLibraryLocation.getItems().addAll(
 	    "Horsens", "Aarhus", "Aalborg", "Copenhagen"
 	    );
 		
-		tbView1BookResults = new TableView();
+		tbView1BookResults = new TableView<SearchResultData>();
 		
 		searchSection = new VBox();
 		innerSearchSection = new HBox();
@@ -121,6 +147,18 @@ public class SearchResultController implements Controller {
 		innerFooterUserActionsSection = new HBox();
 		
 	}
+	
+	
+	
+	public void receiveSearchResults() {
+		
+		bookName = dataOutput.getBookName();
+		bookAuthor = dataOutput.getBookAuthor();
+		bookYear = dataOutput.getBookYear();
+		bookAvailability = dataOutput.getBookAvailability();
+		
+	}
+	
 	
 	public String getTitle() {
 		return "Results from book search";
@@ -152,14 +190,30 @@ public class SearchResultController implements Controller {
 		
 		tf1Search.setPrefColumnCount(34);
 		
-		//\\/\\/\\/\\/\\-=Table Properties=-//\\/\\/\\/\\/\\
+		//\\/\\/\\/\\/\\-=Table Column Properties=-//\\/\\/\\/\\/\\
 
 		bookNameCol1 = new TableColumn("Name");
-		bookAuthorNameCol2 = new TableColumn("Author");
-		bookdateOfIssueCol3 = new TableColumn("Date of issue");
-		bookAvailabilityCol4 = new TableColumn("Availability");
+		bookNameCol1.setPrefWidth(110);
+		bookNameCol1.setCellValueFactory(new PropertyValueFactory<SearchResultData, String>("bookName"));
 		
-		tbView1BookResults.getColumns().addAll(bookNameCol1, bookAuthorNameCol2, bookdateOfIssueCol3, bookAvailabilityCol4);
+		
+		bookAuthorNameCol2 = new TableColumn("Author");
+		bookAuthorNameCol2.setPrefWidth(110);
+		bookAuthorNameCol2.setCellValueFactory(new PropertyValueFactory<SearchResultData, String>("bookAuthor"));
+		
+		bookYearCol3 = new TableColumn("Year");
+		bookYearCol3.setPrefWidth(40);
+		bookYearCol3.setStyle("-fx-alignment: CENTER;");
+		bookYearCol3.setCellValueFactory(new PropertyValueFactory<SearchResultData, String>("bookYear"));
+		
+		bookAvailabilityCol4 = new TableColumn("Available");
+		bookAvailabilityCol4.setPrefWidth(65);
+		bookAvailabilityCol4.setStyle("-fx-alignment: CENTER;");
+		bookAvailabilityCol4.setCellValueFactory(new PropertyValueFactory<SearchResultData, String>("bookAvailability"));
+		
+		
+		tbView1BookResults.setItems(data);
+		tbView1BookResults.getColumns().addAll(bookNameCol1, bookAuthorNameCol2, bookYearCol3, bookAvailabilityCol4);
 		
 		//\\/\\/\\/\\/\\-=Buttons=-//\\/\\/\\/\\/\\
 		
@@ -181,7 +235,7 @@ public class SearchResultController implements Controller {
 				
 		bookListResultsSection.getChildren().addAll(tbView1BookResults);
 
-		bookSearchFiltersSection.getChildren().addAll(cb1BookGenre, cb2BookYearFilter, cb3BookLanguage, cb4BookLibraryLocation);
+		bookSearchFiltersSection.getChildren().addAll(cb1BookGenre, cb2BookYear, cb3BookLanguage, cb4BookLibraryLocation);
 		bookSearchFiltersSection.setPadding(new Insets(0, 5, 0, 0));
 		bookSearchFiltersSection.setSpacing(5);
 
@@ -209,7 +263,71 @@ public class SearchResultController implements Controller {
 		
 		return mainPane;
 	}
+	// Getters for filter selection.
+	public int getCb1BookGenreFilterSelection() {
+		return cb1BookGenre.getSelectionModel().getSelectedIndex();
+	}
 	
+	public int getCb2BookYear() {	
+		return cb2BookYear.getSelectionModel().getSelectedIndex();
+	}
+	
+	public int getCb3BookLanguage() {
+		return cb3BookLanguage.getSelectionModel().getSelectedIndex();
+	}
+	
+	public int getCb4BookLibraryLocation() {
+		return cb4BookLibraryLocation.getSelectionModel().getSelectedIndex();
+	}
+	
+public class SearchResultData {
+		
+		private final SimpleStringProperty bookName;
+		private final SimpleStringProperty bookAuthor;
+		private final SimpleStringProperty bookYear;
+		private final SimpleStringProperty bookAvailability;
+
+		public SearchResultData(String bookName, String bookAuthor, String bookYear, String bookAvailability) {
+			this.bookName = new SimpleStringProperty(bookName);
+			this.bookAuthor = new SimpleStringProperty(bookAuthor);
+			this.bookYear = new SimpleStringProperty(bookYear);
+			this.bookAvailability = new SimpleStringProperty(bookAvailability);
+		}
+
+		public String getBookName() {
+			return bookName.get();
+		}
+
+		public void setBookName(String bkName) {
+			bookName.set(bkName);
+		}
+
+		public String getBookAuthor() {
+			return bookAuthor.get();
+		}
+
+		public void setBookAuthor(String bkAuthor) {
+			bookAuthor.set(bkAuthor);
+		}
+
+		public String getBookYear() {
+			return bookYear.get();
+		}
+		
+		public void setBookYear(int bkYear) {
+			String bookYearHolder = Integer.toString(bkYear);
+			bookYear.set(bookYearHolder);
+		}
+
+		public String getBookAvailability() {
+			return bookAvailability.get();
+		}
+
+		public void setBookAvailability(int bkAvailability) {
+			String bookAvailabilityHolder = Integer.toString(bkAvailability);
+			bookAvailability.set(bookAvailabilityHolder);
+		}
+	}
 
 	@Override
 	public void onWindowOpen(Window win) {
@@ -225,5 +343,4 @@ public class SearchResultController implements Controller {
 	public void onWindowRefresh(Window win) {
 		
 	}
-
 }
