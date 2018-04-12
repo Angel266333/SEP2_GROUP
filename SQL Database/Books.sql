2ae17c4f261cf6bfc45ff186be69f3ce2685eec8
Angel Petrov <angelilianov@gmail.com>
2018-04-12 10:30:49 +0200

    Books table uploaded

---
+++ SQL Database/Books.sql
@@ -0,0 +1,38 @@
+--CLMS SEP2
+
+CREATE SCHEMA CLMS_BOOKS;
+
+SET SEARCH_PATH TO CLMS_BOOKS;
+
+grant usage on schema CLMS_BOOKS to public;
+grant create on schema CLMS_BOOKS to public;
+
+CREATE TABLE Books (
+  bid int NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  isbn VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  image VARCHAR(255) NOT NULL,
+  release VARCHAR(255) NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  PRIMARY KEY(bid),
+
+  UNIQUE(bid)
+);
+
+CREATE TABLE BookInventory (
+	FOREIGN KEY(bid) REFERENCES Books
+	ON DELETE CASCADE
+);
+
+CREATE TABLE BookReservations (
+	FOREIGN KEY(bid) REFERENCES Books
+	ON DELETE CASCADE
+);
+
+CREATE TABLE BookRental (
+	FOREIGN KEY(bid) REFERENCES Books
+	ON DELETE CASCADE
+);
+
+INSERT INTO Books VALUES (1, 'History of the world', 'DK9871443', 'History explained', 'https://...', 'Second', 'Chris Sorensen');
