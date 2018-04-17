3cd5a9a0f32ea1e130f4a67923b95711b496ab2f
Unknown <diego.hisen@gmail.com>
2018-04-17 10:46:29 +0200

    Adding lid to Book and BookRentals

--- src/com/via/clms/shared/BookReservation.java
+++ src/com/via/clms/shared/BookReservation.java
@@ -5,9 +5,11 @@ import java.io.Serializable;
 public class BookReservation implements Serializable {
 	public int bid;
 	public int uid;
+	public int lid;
 
-	public BookReservation(int bid, int uid) {
+	public BookReservation(int bid, int uid, int lid) {
 		this.bid = bid;
 		this.uid = uid;
+		this.lid = lid;
 	}
 }
