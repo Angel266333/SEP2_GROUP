3b2a25f509b795192d2b49a70cf3dbb7644418db
Angel Petrov <angelilianov@gmail.com>
2018-04-17 10:06:55 +0200

    First itteration of ILibrary

---
+++ src/com/via/clms/proxy/ILibraryService.java
@@ -0,0 +1,20 @@
+package com.via.clms.proxy;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import com.via.clms.shared.Library;
+
+/**
+ * A library service class that is used to manage libraries
+ */
+
+public interface ILibraryService extends Remote {
+
+	public boolean createLibrary(byte[] reqToken, String name) throws RemoteException;
+	
+	public Library getLibraryByLID(byte[] reqToken, int lid) throws RemoteException;
+	
+	public Library[] getLibraries(byte[] reqToken, int offset, int length) throws RemoteException;
+	
+}
