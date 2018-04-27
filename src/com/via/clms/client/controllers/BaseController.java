110c67c8a503cf4a7ebb7525dc9dd5f1f5386564
Angel Petrov <angelilianov@gmail.com>
2018-04-27 18:04:32 +0200

    BaseController formatting fix

--- src/com/via/clms/client/controllers/BaseController.java
+++ src/com/via/clms/client/controllers/BaseController.java
@@ -2,16 +2,15 @@ package com.via.clms.client.controllers;
 
 import com.via.clms.client.views.Controller;
 import com.via.clms.client.views.StackController;
+
 import javafx.scene.Parent;
 import javafx.scene.layout.VBox;
 
 public class BaseController extends StackController {
-
 	private VBox vBox;
 
 	public BaseController(Controller mainController) {
 		super(mainController);
-
 	}
 
 	@Override
@@ -27,15 +26,10 @@ public class BaseController extends StackController {
 
 	@Override
 	public void onStackChanged(Parent view, Controller controller) {
-
 		getWindow().setTitle(controller.getTitle());
-
 		if (vBox.getChildren().size() > 0) {
 			vBox.getChildren().remove(0);
 		}
-
 		vBox.getChildren().add(view);
-
 	}
-
 }
