0f181fb21f37077bedf33b38e002478f5fc60af0
kennethp <kennethpete@gmail.com>
2018-05-17 22:00:10 +0200

    Update ClickListener.java

--- src/com/via/clms/client/controllers/containers/ClickListener.java
+++ src/com/via/clms/client/controllers/containers/ClickListener.java
@@ -8,4 +8,10 @@ public interface ClickListener {
 	 * @param i Index to be used for fetching relevant object from said array.
 	 */
 	void click(int i);
+
+	/**
+	 * Same as click(), but for double clicks.
+	 * @param i
+	 */
+	void doubleClick(int i);
 }
