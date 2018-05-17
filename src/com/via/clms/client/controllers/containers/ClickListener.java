40ea1eef42be41e7fcb5d8bd3ca5858d8c7e0998
kennethp <kennethpete@gmail.com>
2018-05-17 14:26:55 +0200

    Add files via upload

---
+++ src/com/via/clms/client/controllers/containers/ClickListener.java
@@ -0,0 +1,11 @@
+package com.via.clms.client.controllers.containers;
+
+public interface ClickListener {
+	/**
+	 * When used in a ClickableTable, click() will be called with the index in the
+	 * array used in populate(), which corresponds to the object presented at the clicked
+	 * position in the table.
+	 * @param i Index to be used for fetching relevant object from said array.
+	 */
+	void click(int i);
+}
