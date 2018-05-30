29715efc26a45f13faba625792ba8a406a0ad303
Angel Petrov <angelilianov@gmail.com>
2018-05-30 10:23:27 +0200

    LibraryService testing finalized

--- tests/com/via/clms/server/services/LibraryServiceTesting.java
+++ tests/com/via/clms/server/services/LibraryServiceTesting.java
@@ -8,6 +8,7 @@ import java.rmi.RemoteException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -31,39 +32,54 @@ public class LibraryServiceTesting {
 		token = userService.getSpecialToken(0, IUserService.ROLE_ADMIN);
 		
 		DatabaseService db = (DatabaseService) ServiceManager.getService("database");
-		int res = db.execute("INSERT INTO Libraries (cLid, cName, cLocation) VALUES (?,?,?)",
-				0,
-				"CLMS",
-				"Horsens");
+		db.execute("INSERT INTO Libraries (cName, cLocation) VALUES (?,?)",
+				"TestLib7771",
+				"SOF");
 		
-		if (res <= 0) {
-			Assert.fail("Failed to setup test user in the database");
-		}
-		
-		ResultSet result = db.query("SELECT cLid FROM Libraries WHERE cLid = 0");
+		ResultSet result = db.query("SELECT cName FROM Libraries WHERE cName = 'TestLib7771'");
 		
 		if (!result.next()) {
 			Assert.fail("Could not retrieve lid from test library in database");
 		}
 	}
 	
-	@Test
-	public void testCreateLibrary() throws RemoteException {	
-	UserService userService = (UserService) ServiceManager.getService("user");
-	ILibraryService libraryService = (LibraryService) ServiceManager.getService("library");
-	boolean result = libraryService.createLibrary(token, "CLMS", "Horsens");
-	assertEquals(true, result);
+	@AfterClass
+	public static void deinitialize() throws SQLException {
+		
+		DatabaseService db = (DatabaseService) ServiceManager.getService("database");
+		db.execute("DELETE FROM Libraries WHERE cName = 'TestLib773'");
+		db.execute("DELETE FROM Libraries WHERE cName = 'TestLib7771'");
+		ServerInstantiator.deinitialize();
 	}
 	
 	@Test
-	public void testGetLibraryByID() throws RemoteException {
+	public void alphaCreateLibrary() throws RemoteException {	
 	ILibraryService libraryService = (LibraryService) ServiceManager.getService("library");
-	Library result = libraryService.getLibraryByLID(token, 0);
+	boolean resultCreate = libraryService.createLibrary(token, "TestLib773", "FantasyLand");
+	assertEquals(true, resultCreate);
+	System.out.println("Alpha: " + resultCreate);
+	}
+	
+	@Test
+	public void betaDeleteLibrary() throws RemoteException {
+	ILibraryService libraryService = (LibraryService) ServiceManager.getService("library");
+	boolean resultDelete = libraryService.deleteLibrary(1);
+	System.out.println("Beta: " + resultDelete);
+	assertEquals(true, resultDelete);
+	}
+	
+	@Test
+	public void gammaGetLibraryByID() throws RemoteException {
+	ILibraryService libraryService = (LibraryService) ServiceManager.getService("library");
+	Library result = libraryService.getLibraryByLID(token, 1);
+	System.out.println("Gamma: " + result);
 	assertTrue(result != null);
 	}
-	
 	@Test
-	public void testGetLibraries() throws RemoteException {
-		
+	public void deltaGetLibraries() throws RemoteException {
+	ILibraryService libraryService = (ILibraryService) ServiceManager.getService("library");
+	Library[] libraries = libraryService.getLibraries(token, 0, 1);
+	System.out.println("Delta: " + libraries);
+	assertTrue(libraries != null);
 	}
 }
