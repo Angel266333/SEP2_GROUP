0dc5658cedf8691ca55377b29ac4e20c9879b40a
Daniel Bergløv <d.bergloev@gmail.com>
2018-05-31 11:06:23 +0200

    Add CPR to session object

--- src/com/via/clms/client/controllers/containers/UserSession.java
+++ src/com/via/clms/client/controllers/containers/UserSession.java
@@ -9,13 +9,17 @@ public class UserSession {
 	public final int lid;
 	
 	/** * */
+	public final int cpr;
+	
+	/** * */
 	public final byte[] token;
 	
 	/**
 	 * 
 	 */
-	public UserSession(byte[] token, int lid) {
+	public UserSession(byte[] token, long cpr, int lid) {
 		this.lid = lid;
 		this.token = token;
+		this.cpr = cpr;
 	}
 }
