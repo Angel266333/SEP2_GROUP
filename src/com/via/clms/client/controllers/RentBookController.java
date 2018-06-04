f8831da2f0e7fdf61f1a4fb79b7880a3017064be
kennethp <kennethpete@gmail.com>
2018-06-04 08:17:06 +0200

    Update RentBookController.java

--- src/com/via/clms/client/controllers/RentBookController.java
+++ src/com/via/clms/client/controllers/RentBookController.java
@@ -1,23 +1,35 @@
 package com.via.clms.client.controllers;
 
+import com.via.clms.client.ServiceManager;
+import com.via.clms.client.controllers.containers.UserSession;
 import com.via.clms.client.views.Controller;
 import com.via.clms.client.views.Window;
 
+import com.via.clms.proxy.IInventoryService;
+import com.via.clms.proxy.IUserService;
+import com.via.clms.shared.Book;
+import com.via.clms.shared.User;
+import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 
+import java.rmi.RemoteException;
+import java.sql.Date;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class RentBookController implements Controller {
-	private BorderPane mainPane;
-	private TextField scanText;
-	private Button rentButton;
-	private Label titleLabel;
-	private Label dateLabel;
-	private TextArea titleTextArea;
-	private Label dateTextLabel;
+	UserSession session;
+	User currentUser;
+	TextArea outputArea;
+
+	public RentBookController(UserSession session) {
+		this.session = session;
+	}
 
 	@Override
 	public String getTitle() {
@@ -26,26 +38,34 @@ public class RentBookController implements Controller {
 
 	@Override
 	public Parent getComponent() {
-		mainPane = new BorderPane();
-		scanText = new TextField();
-		scanText.setPrefColumnCount(20);
-		rentButton = new Button("Rent");
-		titleLabel = new Label("Title");
-		titleLabel.setStyle("-fx-font-weight: bold");
-		dateLabel = new Label("Return date");
-		dateLabel.setStyle("-fx-font-weight: bold");
-		dateTextLabel = new Label();
-		titleTextArea = new TextArea();
-		titleTextArea.setPrefColumnCount(20);
-		titleTextArea.setEditable(false);
+		try {
+			currentUser = ((IUserService) ServiceManager.getService("user")).getUserByCPR(session.token, session.cpr);
+		} catch(RemoteException e) {
+			return new Label("Failed to load user");
+		}
+		GridPane mainPane = new GridPane();
 
-		HBox upperBox = new HBox(scanText, rentButton);
-		VBox middleBox = new VBox(titleLabel, titleTextArea);
-		HBox bottomBox = new HBox(dateLabel, dateTextLabel);
+		TextField isbnField = new TextField();
+		isbnField.setPrefColumnCount(20);
 
-		mainPane.setTop(upperBox);
-		mainPane.setCenter(middleBox);
-		mainPane.setBottom(bottomBox);
+		Button rentButton = new Button("Rent");
+
+		outputArea = new TextArea();
+		outputArea.setPrefRowCount(10);
+		outputArea.setPrefColumnCount(20);
+		outputArea.setEditable(false);
+
+		mainPane.addRow(0, new Label("ISBN"), isbnField, rentButton);
+		mainPane.add(new Separator(Orientation.HORIZONTAL), 0, 1, 3, 1);
+		mainPane.add(outputArea, 0, 2, 3, 1);
+
+		rentButton.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				rentBook(isbnField.getText());
+			}
+		});
+
 		return mainPane;
 	}
 
@@ -72,5 +92,34 @@ public class RentBookController implements Controller {
 	@Override
 	public void onWindowPause(Window win) {
 		
+	}
+
+	private void rentBook(String isbn) {
+		try {
+			IInventoryService service = (IInventoryService) ServiceManager.getService("inventory");
+			Book book = service.getBookByISBN(session.token, session.lid, isbn);
+			if(book == null) {
+				outputArea.appendText("Failed to find book\n");
+				return;
+			}
+			int i = service.addRental(session.token, session.lid, book.bid, currentUser.uid);
+			if(i < 1) {
+				outputArea.appendText("Book could not be rented.\nContact staff");
+				return;
+			}
+			outputArea.appendText("You rented: \n" + book.title + "\nAuthor: " + book.author + "\nISBN: " + book.ISBN + "\nReturn before: " + getReturnDate());
+		} catch(RemoteException e) {
+			outputArea.appendText("Connection error.\n");
+		}
+	}
+
+	private String getReturnDate() {
+		Calendar cal = Calendar.getInstance();
+		long now = Instant.now().getEpochSecond();
+		cal.setTime(Date.from(Instant.ofEpochSecond(now + 2678400L)));//30 days rental counting the day of rental
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		String month = cal.getDisplayName(Calendar.MONTH, Calendar.SHORT_FORMAT, Locale.ENGLISH);
+		int year = cal.get(Calendar.YEAR);
+		return "" + day + "-" + "-" + month + "-" + year;
 	}
 }
